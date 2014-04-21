06797e239b1ac735f806f5489213fa66a5816f92
Sergey Melekhin <sergey@emply.ru>
2014-04-21 18:36:28 +1100

    Added more debug

--- beaker/cache.py
+++ beaker/cache.py
@@ -563,12 +563,13 @@ def _cache_decorate(deco_args, manager, kwargs, region):
                 key_length = cache_regions[region]['key_length']
             else:
                 key_length = kwargs.pop('key_length', 250)
-            print("!!!!!!AAAARRRRGGGGGGGGGGGGHHHHHHHHHHHHHH!!!!!!!!")
-            print("cache_key={} : len={}".format(str(cache_key),str(len(cache_key))))
+
             if len(cache_key) + len(namespace) > int(key_length):
                 if util.py3k:
                     cache_key = cache_key.encode('utf-8')
                 cache_key = sha1(cache_key).hexdigest()
+            print("!!!!!!AAAARRRRGGGGGGGGGGGGHHHHHHHHHHHHHH!!!!!!!!")
+            print("cache_key={} : len={}".format(str(cache_key),str(len(cache_key))))
 
             def go():
                 return func(*args)
